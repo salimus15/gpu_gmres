218b7a85809380e45f29ffbe7f4cf2118a9e4ce6
salimus15 <snahi015@gmail.com>
2015-09-14 13:26:08 +0200

    eeee

--- lhadja.h
+++ lhadja.h
@@ -4,7 +4,7 @@
 class ART
 {
 public:
-    ART():value = -1;
+ //   ART():value = -1;
     int getValue() const { return this->value; } // définition au sein de la classe
     void setValue(int valuer);
     void print() const;
